140d1fbde9941829968c90bcd41da86a77b2c45b
Exequiel Melgarejo <exemax2014@gmail.com>
2026-04-16 10:40:01 -0300

    se modifico el css para mejorar el respondive del navbar  ademas de modificaron las ruutas

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'pages.index')->name('home');
-Route::view('/quienes-somos', 'pages.quienes-somos')->name('quienes-somos');
-Route::view('/comercializacion', 'pages.comercializacion')->name('comercializacion');
-Route::view('/contacto', 'pages.contacto')->name('contacto');
-Route::view('/terminos', 'pages.terminos')->name('terminos');
-Route::view('/catalogo', 'pages.catalogo')->name('catalogo');
-Route::view('/consultas', 'pages.consultas')->name('consultas');
-Route::view('/login', 'pages.login')->name('login');
-Route::view('/registro', 'pages.registro')->name('registro');
+Route::get('/', function () {
+    return view('pages.index');
+})->name('home');
+
+Route::get('/quienes-somos', function () {
+    return view('pages.quienes-somos');
+})->name('quienes-somos');
+
+Route::get('/comercializacion', function () {
+    return view('pages.comercializacion');
+})->name('comercializacion');
+
+Route::get('/contacto', function () {
+    return view('pages.contacto');
+})->name('contacto');
+
+Route::get('/terminos', function () {
+    return view('pages.terminos');
+})->name('terminos');
+
+Route::get('/catalogo', function () {
+    return view('pages.catalogo');
+})->name('catalogo');
+
+Route::get('/consultas', function () {
+    return view('pages.consultas');
+})->name('consultas');
+
+Route::get('/login', function () {
+    return view('pages.login');
+})->name('login');
+
+Route::get('/registro', function () {
+    return view('pages.registro');
+})->name('registro');
